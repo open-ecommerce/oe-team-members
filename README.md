702bc2da6677691807f3062961acaf6536893018
Eduardo Silva <eduardo@open-ecommerce.org>
2016-04-21 13:07:14 +0100

    adding order and title

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# oe-team-members
+![WP, WordPress, Plugin, Custom Types ](hero-team-members.jpg)
+
+# WordPress Simple content type plugin to manage Team Members
+This is a very simple plugin not even worth it to pubish in the wordpress repository.
+But anyway a good sample if you need to implement custom types.
+Instead of add the custom types in your theme I like the plugin aproach because you can re use it in multiples sites and then update the different releases throug composer.
+
+
+##You can see a demo at:
+http://wp-brixton.oe-lab.tk/
+
+
+##How to change for your own use
+You will find 2 main files:
+
+###oe-team-members.php
+here we add the functionality we want to have and the admin menu
+
+
+###single-team-members.php
+this is the template for the single post page
--- oe-team-members.php
+++ oe-team-members.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  Plugin Name: Team Members
+  Plugin Name: OE Team Members
   Plugin URI: http://open-ecommerce.org/
   Description: Declares a plugin that will create a custom post type team members.
   Version: 1.0
@@ -10,7 +10,6 @@
  */
 
 add_action('init', 'create_team_members');
-
 function create_team_members() {
     register_post_type('team_members', array(
         'labels' => array(
@@ -34,6 +33,7 @@ function create_team_members() {
         'show_in_menu' => true,
         'menu_position' => 5,
         'supports' => array('title', 'editor', 'thumbnail'),
+        'taxonomies' => array('category'),
         'menu_icon' => plugins_url('images/oe-team-members.png', __FILE__),
         'query_var' => true,
         'rewrite' => array('slug' => 'member'),
@@ -45,46 +45,78 @@ function create_team_members() {
 }
 
 add_action('admin_init', 'my_admin');
-
 function my_admin() {
     add_meta_box('team_members_meta_box', 'Member Details', 'display_team_members_meta_box', 'team_members', 'normal', 'high');
 }
 
+
+
+add_action('save_post', 'add_team_members_fields', 10, 2);
 function display_team_members_meta_box($team_members) {
+    $team_members_order = get_post_meta($team_members->ID, 'team_members_order', true);
     $team_members_title = get_post_meta($team_members->ID, 'team_members_title', true);
-
     ?>
-
     <div class="small-12 large-8 columns">
         <table>
             <tr>
-                <td style="width: 150px">Title (possition)</td>
+                <td style="width: 70px">Order</td>
                 <td>
-                    <input type="text" size="80"
+                    <input type="text" size="1"
+                        name="team_members_order"
+                        value="<?php echo $team_members_order; ?>" />
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 70px">Org. Title</td>
+                <td>
+                    <input type="text" size="40"
                         name="team_members_title"
                         value="<?php echo $team_members_title; ?>" />
                 </td>
             </tr>
     </table>
     </div>
-
-
     <?php
 }
 
-add_action('save_post', 'add_team_members_fields', 10, 2);
 
-function add_team_members_fields($team_members_id, $team_members) {
-    if ($team_members->post_type == 'team_members') {
-        if (isset($_POST['team_members_title']) &&
-                $_POST['team_members_title'] != '') {
-            update_post_meta($team_members_id, 'team_members_title', esc_html($_POST['team_members_title']));
-        }
+
+add_filter('manage_team_members_posts_columns' , 'add_team_members_columns');
+function add_team_members_columns($columns) {
+    return array_merge($columns,
+              array('team_members_title' => __('Org. Title'),
+                    'team_members_order' =>__( 'Order')));
+}
+
+
+add_action( 'manage_team_members_posts_custom_column' , 'my_custom_team_members_column' );
+function my_custom_team_members_column( $column, $post_id ) {
+    global $post;
+
+    switch ( $column ) {
+      case 'team_members_title':
+        $orgtitle = get_post_meta( $post->ID , 'team_members_title' , true );
+        if ( empty( $orgtitle ) )
+            echo __( 'Not Set' );
+            echo $orgtitle;
+          break;
+        case 'team_members_order':
+            $order = get_post_meta( $post->ID, 'team_members_order', true );
+            if ( empty( $order ) )
+              echo __( 'Not Set' );
+              echo $order;
+            break;
     }
 }
 
-add_filter('template_include', 'include_template_function', 1);
+add_filter( 'manage_edit-team_members_sortable_columns', 'team_register_sortable_columns' );
+function team_register_sortable_columns( $columns ) {
+    $columns['team_members_order'] = 'Order';
+    return $columns;
+}
 
+
+add_filter('template_include', 'include_template_function', 1);
 function include_template_function($template_path) {
     if (get_post_type() == 'team_members') {
         if (is_single()) {
